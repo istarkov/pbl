94b8b5810946a5e66f67a0bf88c06e4460c438dd
cybice <istarkov@gmail.com>
2016-12-23 14:45:42 +0300

    Fix

--- example/server/index.js
+++ example/server/index.js
@@ -9,10 +9,10 @@ const app = express();
 const server = Server(app);
 
 app.use(compression());
-app.use(express.static(path.join(__dirname, './build')));
+app.use(express.static(path.join(__dirname, '../build')));
 
 app.get('/*', (req, res) => {
-  res.sendFile(path.join(__dirname, './build', 'index.html'));
+  res.sendFile(path.join(__dirname, '../build', 'index.html'));
 });
 
 server.listen(process.env.WS_PORT || WS_PORT);
